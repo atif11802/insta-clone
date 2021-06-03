e0bc140b102d6f86164b13a29a4e43b8785897e3
atif <ratul0947@gmail.com>
2021-06-03 12:22:59 +0600

    github host

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Input } from "@material-ui/core";
 import Imageupload from "./Imageupload";
-
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 function getModalStyle() {
 	const top = 50;
@@ -59,29 +59,29 @@ function App() {
 		};
 	}, [user, username]);
 
-	console.log(user)
+	console.log(user);
 
 	useEffect(() => {
-		db.collection("posts").orderBy('timestamp', 'desc').onSnapshot((snapshot) => {
-			setPosts(
-				snapshot.docs.map((doc) => ({
-					id: doc.id,
-					post: doc.data(),
-				}))
-			);
-		});
+		db.collection("posts")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				setPosts(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						post: doc.data(),
+					}))
+				);
+			});
 	}, []);
 
-  const signIn = (e)=>{
-    e.preventDefault();
-    auth
-    .signInWithEmailAndPassword(email,password)
-    .catch((error)=>
-    alert(error.message)
-    )
-    setOpenSignIn(false)
-
-  }
+	const signIn = (e) => {
+		e.preventDefault();
+		auth.signInWithEmailAndPassword(
+			email,
+			password
+		).catch((error) => alert(error.message));
+		setOpenSignIn(false);
+	};
 
 	const signUp = (e) => {
 		e.preventDefault();
@@ -92,21 +92,11 @@ function App() {
 				});
 			})
 			.catch((error) => alert(error.message));
-      setOpen(false);
+		setOpen(false);
 	};
-	
-	
-	
-
-
 
 	return (
 		<div className="app">
-
-      
-
-
-
 			<Modal open={open} onClose={() => setOpen(false)}>
 				<div
 					style={modalStyle}
@@ -164,7 +154,10 @@ function App() {
 				</div>
 			</Modal>
 
-      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+			<Modal
+				open={openSignIn}
+				onClose={() => setOpenSignIn(false)}
+			>
 				<div
 					style={modalStyle}
 					className={classes.paper}
@@ -177,7 +170,7 @@ function App() {
 								alt="Instagram"
 							/>
 						</center>
-						
+
 						<Input
 							placeholder="email"
 							type="text"
@@ -217,70 +210,70 @@ function App() {
 					className="app__headerImage"
 					alt="Instagram"
 				/>
-        
-        {user ? (
-				<Button
-					type="submit"
-					onClick={() => auth.signOut()}
-				>
-					Log out
-				</Button>
-			) :
 
-      
-      
-      (
-        <div className="login__container">
+				{user ? (
+					<Button
+						type="submit"
+						onClick={() => auth.signOut()}
+					>
+						Log out
+					</Button>
+				) : (
+					<div className="login__container">
+						<Button
+							variant="contained"
+							onClick={() =>
+								setOpenSignIn(
+									true
+								)
+							}
+						>
+							sign in
+						</Button>
 
-<Button
-					variant="contained"
-					
-					onClick={() => setOpenSignIn(true)}
-				>
-					sign in
-				</Button>
-
-        <Button
-					variant="contained"
-					
-					onClick={() => setOpen(true)}
-				>
-					sign up
-				</Button>
-      </div>
-				
-			)}
-      
-      
+						<Button
+							variant="contained"
+							onClick={() =>
+								setOpen(true)
+							}
+						>
+							sign up
+						</Button>
+					</div>
+				)}
 			</div>
-      {
-        user?.displayName ? 
-        (<Imageupload username={user.displayName} />)
-        :(
-          ''
-        )
-      }
+			{user?.displayName ? (
+				<Imageupload username={user.displayName} />
+			) : (
+				""
+			)}
 
-			<div className="app__posts">
-     {
-		  posts.map(({ id, post }) => (
-			<Post
-			
-				code={id}
-				key={id}
-				image={post.imageUrl}
-				username={post.username}
-				caption={post.caption}
-			/>
-		))}
-	  
-	  
-		
-      </div>
-
-			
-
-			
+			<SkeletonTheme color="#202020" highlightColor="#444">
+				<div
+					className="app__posts"
+					style={{ fontSize: 20, lineHeight: 2 }}
+				>
+					{posts ? (
+						posts.map(({ id, post }) => (
+							<Post
+								code={id}
+								key={id}
+								image={
+									post.imageUrl
+								}
+								username={
+									post.username
+								}
+								caption={
+									post.caption
+								}
+							/>
+						))
+					) : (
+						<Skeleton duration={2} />
+					)}
+				</div>
+			</SkeletonTheme>
 		</div>
 	);
 }
